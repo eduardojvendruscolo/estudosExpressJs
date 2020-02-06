2e834490a9d88b73fc1e3904ad8303847d3798e5
Eduardo Vendruscolo <eduardojvendruscolo@gmail.com>
2020-02-06 16:41:05 -0200

    add tabela

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 var express = require('express');
+const Sequelize = require('sequelize');
+
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('hello word '+new Date());
+    
+
+    var sequelize = new Sequelize('coagril_ee_dd_pgsql_006', 'cooperate', 'cooperate', {
+        host: 'pgsql01.datacoper.com.br',
+        dialect: 'postgres'
+    });
+
+    const Marca = sequelize.define('marca', {
+        idmarca: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            primaryKey: true
+          },
+          descricao: {
+            type: Sequelize.STRING
+          },
+          codigo: {
+            type: Sequelize.INTEGER
+          },
+          idgrupoempresarial: {
+            type: Sequelize.INTEGER
+          }   
+      }, {
+        freezeTableName: true,
+        timestamps: false
+      });    
+
+    Marca.findAll().then(marca => {
+        var texto = JSON.stringify(marca, null, 4);        
+        res.send(texto);
+      });        
+
 });
 
-app.listen(3000, function() {
-    console.log('example app listening on port 3000!');
+app.listen(3001, function() {
+    console.log('example app listening on port 3001!');
 });
